52c8501bba910549d39177d05b000e8c4ce5ccc2
luckywitantra <iceman.sum41@gmail.com>
2026-05-18 02:38:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1358,14 +1358,12 @@ const superApp = {
         if (modalWa) {
             modalWa.classList.remove('hidden');
             modalWa.classList.add('flex');
-            
-            // --- INI SAKLAR ANIMASI YANG TERLUPA (Tambahkan 4 baris ini) ---
+    
             const modalContent = document.getElementById('modal-wa-confirm-content');
             if (modalContent) {
                 setTimeout(() => modalContent.classList.add('modal-enter-active'), 10);
             }
-            // ---------------------------------------------------------------
-            
+       
             const title = modalWa.querySelector('h3');
             const desc = modalWa.querySelector('p');
             if(title) title.innerText = "Laporan Siap!";

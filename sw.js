89451c2835962f71e3bb93afd8a81fbd35384f5c
luckywitantra <iceman.sum41@gmail.com>
2026-07-29 04:38:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🛑 ATURAN EMAS 1: Setiap upload versi baru ke GitHub/Server, NAIKKAN ANGKA VERSI INI!
-const CACHE_NAME = 'aisnack-erp-v596';
+const CACHE_NAME = 'aisnack-erp-v597';
 
 // 🚀 PERBAIKAN 2: Masukkan app.js dan Ikon PWA ke dalam daftar instalasi wajib
 const urlsToCache = [

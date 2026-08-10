9220cc647b8c253eb1b6bf058ea0466f651d78bd
luckywitantra <iceman.sum41@gmail.com>
2026-08-10 18:58:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🛑 ATURAN EMAS 1: Setiap upload versi baru ke GitHub/Server, NAIKKAN ANGKA VERSI INI!
-const CACHE_NAME = 'aisnack-erp-v703'; // <-- Contoh dinaikkan ke 605
+const CACHE_NAME = 'aisnack-erp-v704'; // <-- Contoh dinaikkan ke 605
 
 const urlsToCache = [
   './index.html',
